54b041240a9f57bda224315aa6ebb5b06fee5691
Martin Dhenu <martin.dhenu@chefkoch.de>
2021-11-18 15:39:09 +0100

    Cut and organize templates

--- assets/app.js
+++ assets/app.js
@@ -7,17 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/scss/sb-admin-2.scss';
-import './styles/app.css';
 
 // start the Stimulus application
 //import './bootstrap';
-
-const $ = require('jquery');
-// this "modifies" the jquery module: adding behavior to it
-// the bootstrap module doesn't export/return anything
-require('bootstrap');
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
-});
-
-require('@fortawesome/fontawesome-free')
+import './js/sb-admin-2';
