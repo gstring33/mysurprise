9d667e9bb7d5c5b19568500453d6a70cc9b16e0b
Martin Dhenu <martin.dhenu@chefkoch.de>
2021-11-28 09:57:11 +0100

    Implement login redirection + template

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,16 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
     /**
-     * @Route("/login", name="login")
+     * @Route("/login", name="app.login")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
      */
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('security/index.html.twig', [
-            'controller_name' => 'LoginController',
+            'last_username' => $lastUsername,
+            'error' => $error,
         ]);
     }
 }
