535317a724ea8b6b547d2128feccc3c2a0ffb827
Martin Dhenu <martin.dhenu@chefkoch.de>
2021-11-30 10:58:24 +0100

    Rename Main Controller

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MainController extends AbstractController
+class AppController extends AbstractController
 {
     /**
      * @Route("/app", name="app.home")
